42fe504034727e9dd13711ff53c4d8796293926d
Elham <elham.harirpoush@arm.com>
2022-06-30 10:11:25 -0400

    add inference server with armnn backend

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import os
 from flask import Flask, request, send_from_directory, render_template
-import tflite_runtime.interpreter as tflite
 import numpy as np
 from PIL import Image
 from werkzeug.utils import secure_filename
-import time
+import tritonclient.http as tritonhttpclient
 
 
 ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png'])
@@ -13,24 +12,20 @@ UPLOAD_FOLDER = 'uploads'
 input_mean = 127.5
 input_std = 127.5
 
+VERBOSE = False
+input_name = 'input'
+input_shape = (1, 244, 244, 3)
+input_dtype = 'FP32'
+output_name = 'MobilenetV1/Predictions/Reshape_1'
+model_name = 'tflite_model'
+url = 'localhost:8000'
+model_version = '1'
+label_path = './models/tflite_model/labels.txt'
+
 
 def load_labels(filename):
     with open(filename, 'r') as f:
         return [line.strip() for line in f.readlines()]
-
-
-# load model and allocate tensors
-def load_model(model_path):
-    armnn_delegate = tflite.load_delegate('./libarmnnDelegate.so.25',
-                                          options={
-                                              "backends": "CpuAcc, CpuRef, GpuAcc ",
-                                              "logging-severity": "info"
-                                          }
-                                          )
-    model = tflite.Interpreter(model_path, experimental_delegates=[armnn_delegate])
-    model.allocate_tensors()
-
-    return model
 
 
 def allowed_file(filename):
@@ -38,39 +33,32 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
-def predict(file):
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
+def predict(img_path):
+    triton_client = tritonhttpclient.InferenceServerClient(url=url, verbose=VERBOSE)
+    model_metadata = triton_client.get_model_metadata(model_name=model_name, model_version=model_version)
+    model_config = triton_client.get_model_config(model_name=model_name, model_version=model_version)
 
-    floating_model = input_details[0]['dtype'] == np.float32
+    height = input_shape[1]
+    width = input_shape[2]
 
-    height = input_details[0]['shape'][1]
-    width = input_details[0]['shape'][2]
-
-    img = Image.open(file).resize((width, height))
+    img = Image.open(img_path).resize((width, height))
     # add N dim
     input_data = np.expand_dims(img, axis=0)
+    input_data = (np.float32(input_data) - input_mean) / input_std
+    input0 = tritonhttpclient.InferInput(input_name, input_shape, input_dtype)
+    input0.set_data_from_numpy(input_data, binary_data=False)
 
-    if floating_model:
-        input_data = (np.float32(input_data) - input_mean) / input_std
+    with open(label_path, 'r') as f:
+        labels = [line.strip() for line in f.readlines()]
 
-    interpreter.set_tensor(input_details[0]['index'], input_data)
+    output = tritonhttpclient.InferRequestedOutput(output_name, binary_data=False)
 
-    interpreter.invoke()
-    start_time = time.time()
-    interpreter.invoke()
-    stop_time = time.time()
-    time_ms = round((stop_time-start_time) * 1000, 2)
+    response = triton_client.infer(model_name, model_version=model_version,
+                                   inputs=[input0], outputs=[output])
 
-    output_data = interpreter.get_tensor(output_details[0]['index'])
-    results = np.squeeze(output_data)
-
-    top_k = results.argsort()[-5:][::-1]
-    labels = load_labels('labels.txt')
-    if floating_model:
-        return labels[top_k[0]], time_ms
-    else:
-        return labels[top_k[0]], time_ms
+    logits = response.as_numpy(output_name)
+    logits = np.asarray(logits, dtype=np.float32)
+    return labels[np.argmax(logits)]
 
 
 App = Flask(__name__)
@@ -88,10 +76,9 @@ def upload_file():
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file_path = os.path.join(App.config['UPLOAD_FOLDER'], filename)
-            file.save(file_path)
-            output, inference_time = predict(file_path)
-    return render_template("home.html", label=output, label_=inference_time, imagesource=file_path)
+            img_file_path = os.path.join(App.config['UPLOAD_FOLDER'], filename)
+            output = predict(img_file_path)
+    return render_template("home.html", label=output, imagesource=img_file_path)
 
 
 @App.route('/uploads/<filename>')
@@ -101,8 +88,5 @@ def uploaded_file(filename):
 
 
 if __name__ == "__main__":
-
-    interpreter = load_model('mobilenet_v1_1.0_224_quant.tflite')
     port = int(os.environ.get('PORT', 8080))
     App.run(debug=False, host='0.0.0.0', port=port)
-    
