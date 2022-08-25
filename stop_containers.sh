ff13c5f145a925239fba98a0721402c510615f53
Elham Harirpoush <elham.harirpoush@arm.com>
2022-08-25 14:18:19 -0400

    Adding License

--- stop_containers.sh
+++ stop_containers.sh
@@ -1,3 +1,8 @@
+#
+# SPDX-FileCopyrightText: Copyright 2022 Arm Limited and/or its affiliates <open-source-office@arm.com>
+# SPDX-License-Identifier: MIT
+#
+
 ids=$(docker ps -a -q)
 for id in $ids
 do
