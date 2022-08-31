543bfe588641673ff09ba246f82f623848a035b7
Elham Harirpoush <elham.harirpoush@arm.com>
2022-08-31 18:23:28 -0400

    Add License

--- invoke_server.sh
+++ invoke_server.sh
@@ -1,2 +1,6 @@
+#
+# SPDX-FileCopyrightText: Copyright 2022 Arm Limited and/or its affiliates <open-source-office@arm.com>
+# SPDX-License-Identifier: MIT
+#
 #!/bin/bash
 tritonserver --model-repository /models
